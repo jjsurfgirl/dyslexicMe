1f339372852e5f716bf74b8553d8ecb5dacbc5c6
Jenna Connolly <jjsurfgirl@rocketmail.com>
2013-05-08 00:59:50 -0700

    Created a rails application and status updats

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DyslexicMe::Application.config.secret_token = 'ec4f15d960630c708b9eeaae3f04ae5040725f7fc65c248cd559b598bcd775e7976b5d3676f6e044cfa0a9160a33dac0790398ce7a8ef3a015630a9a64f6d7fb'
